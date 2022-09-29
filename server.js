2153ec1f26c92cb3c92a664d69dfa4a4c13373de
bally <bally.lomibao@gmail.com>
2022-09-29 23:14:12 +1300

    set up prod dev

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const http = require('http');
+
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+const PORT = 5000;
+
+
+server.listen(PORT, () => console.log(`Server runnning on port ${PORT}`));
+
+
